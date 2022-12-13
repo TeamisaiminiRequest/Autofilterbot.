ff1770a4ce29cb5a617f63a002a1f783b41a1bc8
TeamisaiminiRequest <120029174+TeamisaiminiRequest@users.noreply.github.com>
2022-12-13 20:05:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 PICS = (environ.get('PICS', 'https://te.legra.ph/file/c8ab76845b90b35d412f9.jpg')).split()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('5901090168', '2001653136').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('5901090168').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('-1001864502180', '0').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '2001653136').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
